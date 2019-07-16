95b92aedd176e61094240bf224d21305a3958c1c
Purushotam Rai <purushotamkrrai@gmail.com>
2019-07-16 22:13:30 +0530

    CC-2: Created PR Parser.

--- code_censor.js
+++ code_censor.js
@@ -1,82 +1,89 @@
-var suggestions = {}
-suggestions.init = {
-  regex: 'function\\s*\\S*_init\\(\\)',
-  category: 'performance',
-  file_types: ['module'],
-  title: 'Avoid using hook_init',
-  description:
-    'hook_init executes at the beginning of every single page request and hence affects performance'
-}
-suggestions.preprocess_field = {
-  regex: 'function\\s*\\S*_preprocess_field\\(',
-  category: 'performance',
-  file_types: ['module', 'theme'],
-  title: 'Avoid using hook_preprocess_field',
-  description:
-    'If possible go with specific preprocessor like <a href="https://api.drupal.org/api/drupal/core!modules!system!templates!field.html.twig/8.2.x">hook_preprocess_field__field_type</a>'
-}
 
-// Implement code review
-var reviewFiles = document.querySelectorAll('div.js-details-container.file')
-reviewFiles.forEach(function (reviewFile) {
-//   let fileType = reviewFile.getAttribute('data-file-type')
-  const fileContent = reviewFile.querySelectorAll(
-    'div.js-file-content table.diff-table tr'
-  )
+/**
+ * Read the PR.
+ */
+const currentPullRequest = parsePullRequest()
 
-  // Start from line 1 and perform suggestion checks.
-  for (let lineNumber = 1; lineNumber < fileContent.length; lineNumber++) {
-    performSuggestionChecks(fileContent[lineNumber], lineNumber)
-  }
-})
+console.log(currentPullRequest)
+// var suggestions = {}
+// suggestions.init = {
+//   regex: 'function\\s*\\S*_init\\(\\)',
+//   category: 'performance',
+//   file_types: ['module'],
+//   title: 'Avoid using hook_init',
+//   description:
+//     'hook_init executes at the beginning of every single page request and hence affects performance'
+// }
+// suggestions.preprocess_field = {
+//   regex: 'function\\s*\\S*_preprocess_field\\(',
+//   category: 'performance',
+//   file_types: ['module', 'theme'],
+//   title: 'Avoid using hook_preprocess_field',
+//   description:
+//     'If possible go with specific preprocessor like <a href="https://api.drupal.org/api/drupal/core!modules!system!templates!field.html.twig/8.2.x">hook_preprocess_field__field_type</a>'
+// }
 
-function performSuggestionChecks (data, lineNumber) {
-  const content = data.querySelector('span.blob-code-inner').innerHTML
+// // Implement code review
+// var reviewFiles = document.querySelectorAll('div.js-details-container.file')
+// reviewFiles.forEach(function (reviewFile) {
+// //   let fileType = reviewFile.getAttribute('data-file-type')
+//   const fileContent = reviewFile.querySelectorAll(
+//     'div.js-file-content table.diff-table tr'
+//   )
 
-  // Perform review.
-  for (const identifier in suggestions) {
-    const suggestion = suggestions[identifier]
+//   // Start from line 1 and perform suggestion checks.
+//   for (let lineNumber = 1; lineNumber < fileContent.length; lineNumber++) {
+//     performSuggestionChecks(fileContent[lineNumber], lineNumber)
+//   }
+// })
 
-    // if (suggestions.hasOwnProperty(identifier)) {
-    const pattern = new RegExp(suggestion.regex)
-    if (pattern.test(content)) {
-      highlightRow(data, suggestion, lineNumber)
-    }
-    // }
-  }
-}
+// function performSuggestionChecks (data, lineNumber) {
+//   const content = data.querySelector('span.blob-code-inner').innerHTML
 
-// Highlight reviews
-function highlightRow (data, suggestion, lineNumber) {
-  data.className += ' cc-suggest cc-tooltip'
-  // data.querySelectorAll('td').forEach(function(el) {
-  //     el.style.backgroundColor = suggestionColor;
-  // });
-  const emptyCell = data.querySelector('td.blob-num.empty-cell')
+//   // Perform review.
+//   for (const identifier in suggestions) {
+//     const suggestion = suggestions[identifier]
 
-  // Display icon image
-  const suggestionIcon = document.createElement('img')
-  var imgURL = chrome.extension.getURL('assets/icons/' + suggestion.category + '.png')
-  suggestionIcon.className = 'cc-icon cc-' + suggestion.category
-  suggestionIcon.src = imgURL
-  suggestionIcon.alt = suggestion.category
-  emptyCell.appendChild(suggestionIcon)
+//     // if (suggestions.hasOwnProperty(identifier)) {
+//     const pattern = new RegExp(suggestion.regex)
+//     if (pattern.test(content)) {
+//       highlightRow(data, suggestion, lineNumber)
+//     }
+//     // }
+//   }
+// }
 
-  // Create tooltip to show suggestion.
-  const tooltipData = document.createElement('div')
-  tooltipData.className = 'top cc-tooltip-text'
-  var tooltipDataTitle = document.createElement('h3')
-  tooltipDataTitle.innerText = suggestion.title
-  var tooltipDataDesc = document.createElement('p')
-  tooltipDataDesc.innerHTML = suggestion.description
-  tooltipData.appendChild(tooltipDataTitle)
-  tooltipData.appendChild(tooltipDataDesc)
-  tooltipData.appendChild(document.createElement('i'))
-  data.innerHTML += tooltipData.outerHTML
+// // Highlight reviews
+// function highlightRow (data, suggestion, lineNumber) {
+//   data.className += ' cc-suggest cc-tooltip'
+//   // data.querySelectorAll('td').forEach(function(el) {
+//   //     el.style.backgroundColor = suggestionColor;
+//   // });
+//   const emptyCell = data.querySelector('td.blob-num.empty-cell')
 
-  // Position the tooltip.
-  data.addEventListener('mouseover', function (event) {
-    const tooltip = data.querySelector('.cc-tooltip-text')
-    tooltip.style.top = lineNumber * 20 + ' px'
-  })
-}
+//   // Display icon image
+//   const suggestionIcon = document.createElement('img')
+//   var imgURL = chrome.extension.getURL('assets/icons/' + suggestion.category + '.png')
+//   suggestionIcon.className = 'cc-icon cc-' + suggestion.category
+//   suggestionIcon.src = imgURL
+//   suggestionIcon.alt = suggestion.category
+//   emptyCell.appendChild(suggestionIcon)
+
+//   // Create tooltip to show suggestion.
+//   const tooltipData = document.createElement('div')
+//   tooltipData.className = 'top cc-tooltip-text'
+//   var tooltipDataTitle = document.createElement('h3')
+//   tooltipDataTitle.innerText = suggestion.title
+//   var tooltipDataDesc = document.createElement('p')
+//   tooltipDataDesc.innerHTML = suggestion.description
+//   tooltipData.appendChild(tooltipDataTitle)
+//   tooltipData.appendChild(tooltipDataDesc)
+//   tooltipData.appendChild(document.createElement('i'))
+//   data.innerHTML += tooltipData.outerHTML
+
+//   // Position the tooltip.
+//   data.addEventListener('mouseover', function (event) {
+//     const tooltip = data.querySelector('.cc-tooltip-text')
+//     tooltip.style.top = lineNumber * 20 + ' px'
+//   })
+// }
