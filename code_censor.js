7b82e10e24aa3dcbbe70420213102c242eddf1ac
Gaurav Goyal <gkg.ras@gmail.com>
2019-08-20 22:44:37 +0530

    Fixing code sensor for ajaxified.:

--- code_censor.js
+++ code_censor.js
@@ -27,3 +27,31 @@ if (filesUrl.test(url)) {
     // Execute the test script now.
     startCodeCensor();
 }
+
+let debounce;
+// Observe progressively loaded diff content
+Array.from(document.querySelectorAll(`
+  .js-diff-progressive-container,
+  .js-diff-load-container`
+)).forEach(target => {
+  new MutationObserver(mutations => {
+    mutations.forEach(mutation => {
+      // preform checks before adding code wrap to minimize function calls
+      const tar = mutation.target;
+      if (tar && (
+        tar.classList.contains("js-diff-progressive-container") ||
+        tar.classList.contains("js-diff-load-container") ||
+        tar.classList.contains("blob-wrapper")
+      )
+      ) {
+        clearTimeout(debounce);
+        debounce = setTimeout(() => {
+          startCodeCensor();
+        }, 500);
+      }
+    });
+  }).observe(target, {
+    childList: true,
+    subtree: true
+  });
+});
